e70be1321ad76236122b5a222a1fe92b38982b06
Ruben <ruben@MacBook-Pro-2017-15-Inch.local>
2019-10-13 04:28:16 -0300

    crud simple

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-clientes='pablo,ricardo,'
+import sys
+
+
+clientes = 'pablo,ricardo,'
+
 
 def crear_clientes(nombre):
     global clientes
@@ -8,29 +12,82 @@ def crear_clientes(nombre):
     else:
         print('El cliente ya esta en la lista de clientes')
 
+
 def listar_clientes():
     global clientes
     print(clientes)
 
+
 def _agregar_coma():
     global clientes
     clientes += ','
+
 
 def _bienvenido():
     print('BIENVENIDO A VENTAS')
     print('*' * 50)
     print('Que quieres hacer hoy?')
     print('[C]rear cliente')
+    print('[A]ctualizar cliente')
     print('[B]orrar cliente')
+    print('[E]ncontrar cliente')
+
+
+def borrar_cliente(nombre):
+    global clientes
+    if nombre in clientes:
+        clientes = clientes.remplace(nombre + ',', '')
+    else:
+        print('No existe ese cliente en nuestra lista')
+
+
+def obtener_cliente():
+    nombre = None
+    while not nombre:
+        nombre = input('Cual es el nombre del cliente que quiere modificar? ')
+        if nombre == 'salir':
+            nombre = None
+            break
+    if not nombre:
+        sys.exit()
+    return nombre
+
+
+def actualizar_cliente(nombre, nombre_nuevo):
+    global clientes
+    if nombre in clientes:
+        clientes = clientes.replace(nombre + ',', nombre_nuevo + ',')
+    else:
+        print('El cliente no esta en la lista de clientes')
+
+
+def encontrar_cliente(nombre):
+    for cliente in clientes.split(','):
+        if cliente != nombre:
+            continue
+        else:
+            return True
+
 
 if __name__ == '__main__':
     _bienvenido()
-    comando = input()
+    comando = input('Ingrese el comando: ').upper()
     if comando == 'C':
-        nombre_cliente = input('Cual es el nombre del cliente? ')
-        crear_clientes(nombre_cliente)
         listar_clientes()
-    elif comando == 'D':
-        pass
+        crear_clientes(obtener_cliente())
+        listar_clientes()
+    elif comando == 'B':
+        borrar_cliente(obtener_cliente())
+        listar_clientes()
+    elif comando == 'A':
+        nombre_nuevo = input('Cual es el nuevo nombre? ')
+        actualizar_cliente(obtener_cliente(), nombre_nuevo)
+        listar_clientes()
+    elif comando == 'E':
+        cliente = encontrar_cliente(obtener_cliente())
+        if cliente:
+            print('El cliente está en la lista')
+        else:
+            print('El cliente no esta en nuestra lista')
     else:
         print('Comando invalido')
